9535450f76a8b0bd79edaa08ee31706a5cd875c0
Denis Gitko <gitkodev@gmail.com>
2024-02-14 15:08:02 +0800

    Edit http handling process

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,24 @@ import (
 
 	"github.com/gitkoDev/pokemon-db/database"
 	"github.com/gitkoDev/pokemon-db/pkg/handlers"
+
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	if err := database.ConnectToDb(); err != nil {
-		log.Fatalln("error connecting to the database:", err)
+	// DB connection phase
+	db, err := database.ConnectToDb()
+	if err != nil {
+		log.Fatalln("error connecting to database:", err)
+	} else {
+		log.Println("connected to database")
 	}
 
+	// Routing phase
 	r := chi.NewRouter()
-	r.Get("/", handlers.TestFunc)
+	r.Get("/", handlers.GetAll(db))
 
-	err := http.ListenAndServe(":8080", r)
-
+	err = http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatalln("error starting the server", err)
 	}
