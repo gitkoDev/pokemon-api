e3b999ff2ac0c07cef9427a26a8980c1cf041d2b
Denis Gitko <gitkodev@gmail.com>
2024-02-15 17:20:50 +0800

    Change sql schema

--- db/migrations/001_pokemon-table.sql
+++ db/migrations/001_pokemon-table.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS pokemon (
     type VARCHAR (100)[] ,
     hp INTEGER,
     attack INTEGER,
-    defense INTEGER
+    defense INTEGER,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 
 -- +goose Down
